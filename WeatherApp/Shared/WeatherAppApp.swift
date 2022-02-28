cc94d53ba62184fe685dbb84f6938fe278e40c68
bunoza <domagoj.bunoza@gmail.com>
2022-02-28 22:54:36 +0100

    Add assets

---
+++ WeatherApp/Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Domagoj Bunoza on 28.02.2022..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
