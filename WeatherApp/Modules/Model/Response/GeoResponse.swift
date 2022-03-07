9e0aea1cc82bc3406cf91708a773b018c64a70bd
bunoza <domagoj.bunoza@gmail.com>
2022-03-07 23:13:56 +0100

    remove unnecessary variables and structures from response models

--- WeatherApp/Modules/Model/Response/GeoResponse.swift
+++ WeatherApp/Modules/Model/Response/GeoResponse.swift
@@ -9,47 +9,19 @@ import Foundation
 
 // MARK: - Welcome
 struct GeoResponse: Codable {
-    let totalResultsCount: Int
     let geonames: [Geoname]
 }
 enum CodingKeys: String, CodingKey {
     case GeoResponse = "Welcome"
-    case totalResultsCount
 }
 
 // MARK: - Geoname
 struct Geoname: Codable {
-//    let adminCode1: String
     let lng: String
-    let geonameID: Int
-    let toponymName, countryID: String
-    let population: Int
-    let countryCode, name: String
-//    let adminCodes1: AdminCodes1
-    let countryName, fcodeName, adminName1, lat: String
-    let fcode: String
+    let name: String
+    let lat: String
 
     enum CodingKeys: String, CodingKey {
-//        case adminCode1
-        case lng
-        case geonameID = "geonameId"
-        case toponymName
-        case countryID = "countryId"
-        case population, countryCode, name, countryName, fcodeName, adminName1, lat, fcode
-//        case adminCodes1
+        case name, lat, lng
     }
-}
-
-// MARK: - AdminCodes1
-//struct AdminCodes1: Codable {
-//    let iso31662: String
-//
-//    enum CodingKeys: String, CodingKey {
-//        case iso31662 = "ISO3166_2"
-//    }
-//}
-
-enum FclName: String, Codable {
-    case cityVillage = "city, village,..."
-    case parksArea = "parks,area, ..."
 }
