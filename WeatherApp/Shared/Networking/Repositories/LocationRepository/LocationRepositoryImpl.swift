7af68812bace5af6bf23ff885fd07d43bf28c854
bunoza <domagoj.bunoza@gmail.com>
2022-03-15 19:18:53 +0100

    remove constants, implement rest endpoints with plist

--- WeatherApp/Shared/Networking/Repositories/LocationRepository/LocationRepositoryImpl.swift
+++ WeatherApp/Shared/Networking/Repositories/LocationRepository/LocationRepositoryImpl.swift
@@ -10,6 +10,6 @@ import Foundation
 class LocationRepositoryImpl : LocationRepository {
     
     func fetch(cityName: String, completion: @escaping (Result<GeoResponse, NetworkError>) -> ()) {
-        RestManager.fetch(url: "http://api.geonames.org/searchJSON?q=\(cityName)&maxRows=10&lang=es&username=\(Constants.username)", completionHandler: completion)
+        RestManager.fetch(url: RestEndpoints.geo(cityName: cityName).endpoint(), completionHandler: completion)
     }
 }
